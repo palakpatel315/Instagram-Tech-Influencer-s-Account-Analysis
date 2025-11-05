18ddd1fa2f603a1a99d6de36890851187d4ac751
palakpatel315 <palakpatel315@gmail.com>
2025-11-05 10:23:41 +0000

    Update Q_2_MAX_MIN_daily_impressions.sql

--- Instagram_Tech_analysis_SQL_queries/Q_2_MAX_MIN_daily_impressions.sql
+++ Instagram_Tech_analysis_SQL_queries/Q_2_MAX_MIN_daily_impressions.sql
@@ -1,3 +1,6 @@
+-- Q_2. What are the highest and lowest recorded impressions for each post type?
+
+
 SELECT post_type,
        MAX(impressions) AS highest_daily_impression,
        MIN(impressions) AS lowest_daily_impression
