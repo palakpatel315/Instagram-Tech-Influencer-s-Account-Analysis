17f426dd4f4225a8b8615a86ab008ae32bcce55c
palakpatel315 <palakpatel315@gmail.com>
2025-11-05 10:24:28 +0000

    Update Q_3_Weekend_data.sql

--- Instagram_Tech_analysis_SQL_queries/Q_3_Weekend_data.sql
+++ Instagram_Tech_analysis_SQL_queries/Q_3_Weekend_data.sql
@@ -1,3 +1,6 @@
+-- Q_3. Filter all the posts that were published on a weekend in the month of March and April and export them to a separate csv file.
+
+
 SELECT fc.*
 FROM fact_content fc
 JOIN dim_dates dd ON fc.date = dd.date
